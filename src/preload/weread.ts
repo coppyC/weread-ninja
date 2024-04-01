553337df00cfe2fd63f90a0ae0b4e954c1e8784c
coppy <zheqingli@icloud.com>
2024-04-01 18:04:08 +0800

    🎈 新特性：按住 F2 拖拽窗口

--- src/preload/weread.ts
+++ src/preload/weread.ts
@@ -3,8 +3,26 @@ import ninjaCSS from './weread-ninja.css?raw'
 
 requestAnimationFrame(function ninjaSneakIn() {
   if (!document.head) return void requestAnimationFrame(ninjaSneakIn)
+  
   const style = document.createElement('style')
   style.innerText = ninjaCSS
   document.head.appendChild(style)
+  
+  const dragBlock = document.createElement('div')
+  dragBlock.id = "ninja-DragQuicklyArea"
+  document.body.appendChild(dragBlock)
+  document.addEventListener("keydown", e => {
+    if (e.key == "F2") {
+      e.preventDefault()
+      dragBlock.classList.add("ninja-DragQuickly")
+    }
+  })
+  document.addEventListener("keyup", e => {
+    if (e.key == "F2") {
+      e.preventDefault()
+      dragBlock.classList.remove("ninja-DragQuickly")
+    }
+  })
+
   console.log("%c微读忍者，启动！", "color:#c82330;font-weight:bold")
 })
